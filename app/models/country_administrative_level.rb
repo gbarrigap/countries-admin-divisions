4c0fad206ad73a838cace3717e60b054823cd259
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-01-21 21:51:49 -0300

    test CountryAdministrativeLevel

--- app/models/country_administrative_level.rb
+++ app/models/country_administrative_level.rb
@@ -2,4 +2,22 @@ class CountryAdministrativeLevel < ApplicationRecord
   belongs_to :country
   has_many :country_administrative_divisions, dependent: :destroy
   has_many :addresses, through: :country_administrative_divisions
+
+  validates :name, presence: true, uniqueness: { scope: :country }
+  validates :international_name, uniqueness: { scope: :country, allow_nil: true }
+
+  before_save :nil_if_blank
+
+  protected
+
+    #
+    # If the international_name is set to a blank value, it is changed to nil,
+    # in order to allow the record to be saved, since non nil blank values,
+    # such as the empty string, will be considered duplicated by the underlying
+    # database unique constraint associated with this column.
+    #
+    def nil_if_blank
+      self.international_name = nil if international_name.blank?
+    end
+
 end
--- test/models/country_administrative_level_test.rb
+++ test/models/country_administrative_level_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class CountryAdministrativeLevelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save country admin level without a country' do
+    level = CountryAdministrativeLevel.new(name: 'Chile')
+    assert_not level.save
+  end
+
+  test 'should not save country admin level without a name' do
+    chile = Country.create(name: 'Chile')
+    level = CountryAdministrativeLevel.new(country: chile)
+    assert_not level.save
+  end
+
+  test 'should not save country admin level with a blank name' do
+    chile = Country.create(name: 'Chile')
+    level = CountryAdministrativeLevel.new(country: chile, name: '')
+    assert_not level.save
+  end
+
+  test 'different levels by country should not have the same administrative level names' do
+    chile = Country.create(name: 'Chile')
+    chilean_first_admin_level = CountryAdministrativeLevel.create(country: chile, level: 1, name: 'Región')
+    chilean_second_admin_level = CountryAdministrativeLevel.new(country: chile, level: 2, name: 'Región')
+    assert_not chilean_second_admin_level.save
+  end
+
+  test 'different levels by country should not have the same administrative level international names' do
+    chile = Country.create(name: 'Chile')
+    chilean_first_admin_level = CountryAdministrativeLevel.create(country: chile, level: 1, name: 'Región', international_name: 'Region')
+    chilean_second_admin_level = CountryAdministrativeLevel.new(country: chile, level: 2, name: 'Provincia', international_name: 'Region')
+    assert_not chilean_second_admin_level.save
+  end
+
+  test 'different countries should be able to have the same administrative level names' do
+    chile = Country.create(name: 'Chile')
+    peru = Country.create(name: 'Perú')
+    chilean_first_admin_level = CountryAdministrativeLevel.create(country: chile, level: 1, name: 'Región')
+    peruvian_first_admin_level = CountryAdministrativeLevel.new(country: peru, level: 1, name: 'Región')
+    assert peruvian_first_admin_level.save
+  end
+
+  test 'different countries should be able to have the same administrative level international names' do
+    chile = Country.create(name: 'Chile')
+    peru = Country.create(name: 'peru')
+    chilean_first_admin_level = CountryAdministrativeLevel.create(country: chile, level: 1, name: 'Región', international_name: 'Region')
+    peruvian_first_admin_level = CountryAdministrativeLevel.new(country: peru, level: 1, name: 'Maakuntaa', international_name: 'Region')
+    assert peruvian_first_admin_level.save
+  end
+
 end
